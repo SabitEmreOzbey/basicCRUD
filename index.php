d6b0ca0850b9bbd99ad5d1a1ab1bc7696a591653
Emre Özbey <50449797+SabitEmreOzbey@users.noreply.github.com>
2020-12-01 00:31:38 +0300

    pagination add

--- index.php
+++ index.php
@@ -39,8 +39,18 @@
 
         <!-- PHP code to read records will be here -->
         <?php
+
         // include database connection
         include 'config/database.php';
+        // PAGINATION VARIABLES
+        // page is the current page, if there's nothing set, default is page 1
+        $page = isset($_GET['page']) ? $_GET['page'] : 1;
+
+        // set records or rows of data per page
+        $records_per_page = 5;
+
+        // calculate for the query LIMIT clause
+        $from_record_num = ($records_per_page * $page) - $records_per_page;
 
         // delete message prompt will be here
         $action = isset($_GET['action']) ? $_GET['action'] : "";
@@ -50,8 +60,17 @@
             echo "<div class='alert alert-success'>Record was deleted.</div>";
         }
         // select all data
-        $query = "SELECT id, name, description, price FROM products ORDER BY id DESC";
+        /*$query = "SELECT id, name, description, price FROM products ORDER BY id DESC";
         $stmt = $con->prepare($query);
+        $stmt->execute();*/
+
+        // select data for current page
+        $query = "SELECT id, name, description, price FROM products ORDER BY id DESC
+                  LIMIT :from_record_num, :records_per_page";
+
+        $stmt = $con->prepare($query);
+        $stmt->bindParam(":from_record_num", $from_record_num, PDO::PARAM_INT);
+        $stmt->bindParam(":records_per_page", $records_per_page, PDO::PARAM_INT);
         $stmt->execute();
 
         // this is how to get number of rows returned
@@ -106,7 +125,23 @@
 
             // end table
             echo "</table>";
+            // PAGINATION
+            // count total number of rows
+            $query = "SELECT COUNT(*) as total_rows FROM products";
+            $stmt = $con->prepare($query);
+
+            // execute query
+            $stmt->execute();
+
+            // get total rows
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            $total_rows = $row['total_rows'];
+            // paginate records
+            $page_url = "index.php?";
+            include_once "paging.php";
         }
+
+
 
         // if no records found
         else {
